5720eb99bbb1e7c4cd9947c52bfb43788357d181
Nishant Chauhan <mailxnishant@gmail.com>
2024-11-08 22:28:42 +0530

    Added Linktree

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <>
             <div className='fixed bottom-0 h-10 w-full flex items-center justify-center bg-white drop-shadow-xl'>
-                <h2 className='font-extrathin text-sm'>© 2024 - Gautam Buddha University | Designed and Developed by <span className='font-bold cursor-pointer'>Nishant</span></h2>
+                <h2 className='font-extrathin text-sm'>© 2024 - Gautam Buddha University | Designed and Developed by <span className='font-bold cursor-pointer'><a href='https://linktr.ee/linkxnishant' target='_blank'>Nishant</a></span></h2>
             </div>
         </>
     )
