b5337dfc935eb3aa2a82d071e21f360bf41c5391
Nishant Chauhan <mailxnishant@gmail.com>
2024-11-08 22:05:56 +0530

    Added Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ReactDOM from 'react-dom/client'
 import './index.css'
-// import Home from './Pages/Home.jsx'
+import Home from './Pages/Home.jsx'
 // import Validation from './Pages/Validation.jsx'
-import Error from './Pages/Error.jsx'
+// import Error from './Pages/Error.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <Home /> */}
+    <Home />
     {/* <Validation /> */}
-    <Error />
+    {/* <Error /> */}
   </StrictMode>
 )
